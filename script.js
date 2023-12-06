ccdadc5e6af4828deeb5c8a358bd4a3405378dbe
reshikastha <shresthareshika@gmail.com>
2023-12-06 14:47:31 +0545

    Logo and projects design

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ function scrollToContact() {
   var contactSection = document.getElementById('contact');
   contactSection.scrollIntoView({ behavior: 'smooth' });
 }
+
+function showTab(tabId) {
+  // Hide all tab contents
+  document.querySelectorAll('.details-container').forEach(function (tabContent) {
+    tabContent.style.display = 'none';
+  });
+
+  // Show the selected tab content or specific contents for "All" and "React Native"
+  if (tabId === 'radio-1') {
+    document.querySelectorAll('.details-container').forEach(function (tabContent) {
+      tabContent.style.display = 'block';
+    });
+  } else if (tabId === 'radio-2') {
+    document.getElementById('tab-content-radio-1').style.display = 'block';
+    document.getElementById('tab-content-radio-2').style.display = 'block';
+  } else {
+    // Show the selected tab content for other tabs
+    const selectedTabContent = document.getElementById(`tab-content-${tabId}`);
+    if (selectedTabContent) {
+      selectedTabContent.style.display = 'block';
+    }
+  }
+}
